0e3513357db062b3034e577c2d4fdcc05d82ddd6
ajlaursen <ajlaursen@aol.com>
2020-11-09 14:02:58 -0500

    starting questions process in JS trying to make for loop work

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-// array of questions for user
-const questions = [
+const inquirer = require( "inquirer" );
 
-];
+    const promptUser = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectName',
+            message: 'What is the title of your project?',
+          },
+          {
+            type: 'number',
+            name: 'contributors',
+            message: 'How many contributors',
+          },
+    ])
+    .then((answers) => {
+        // trying to write a dynamic function that will ask a number of questions based on the user input
+      for (x = 1; x <= answers.contributors; x++){
+          inquirer.prompt([
+              {
+                  type: 'input',
+                  name: `contributor ${x}`,
+                  message: `Name of contributor number ${x}`
+              }
+          ])
+      }
+  })
+    
 
+  
 // function to write README file
 function writeToFile(fileName, data) {
 }
 
 // function to initialize program
 function init() {
-
+    promptUser();
 }
 
 // function call to initialize program
