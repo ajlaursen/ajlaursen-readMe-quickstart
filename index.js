ee41d979bdf1b291858daee03c68e7d538ffda2a
ajlaursen <ajlaursen@aol.com>
2020-11-10 13:52:23 -0500

    put answers into an object the way i wanted

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require( "inquirer" );
-
-    const promptUser = () =>
+let data = {};
+let userArrayLength;
+    const promptUser = () => {
     inquirer.prompt([
         {
             type: 'input',
@@ -14,9 +15,12 @@ const inquirer = require( "inquirer" );
           },
     ])
     .then((answers) => {
+        data.projectName = answers.projectName;
+        console.log(data)
         contributorsArr = [];
         // dynamic function that takes previous answer and creates a number of questions based on the input from before
       for (x = 1; x <= answers.contributors; x++){
+          userArrayLength = x
           contributorsArr.push({
             type: 'input',
             name: `contributor${x}`,
@@ -28,9 +32,16 @@ const inquirer = require( "inquirer" );
             )  
         }
         inquirer.prompt(contributorsArr)
+        .then((answers) => {
+          data = {...data,...answers}
+        console.log(data);
+        })
+      })
+      inquirer.prompt([
         
-        
-  })
+      ])
+    }
+
     
 
   
